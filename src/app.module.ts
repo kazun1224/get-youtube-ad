9dd849c30a549dcd0dfb8c745e644cb1053e0cf4
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-01-13 13:55:06 +0900

    一通りの実行をできるように構築

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { YoutubeModule } from './youtube/youtube.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot(), YoutubeModule],
+  imports: [ConfigModule.forRoot(), YoutubeModule,ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
