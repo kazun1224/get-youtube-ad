1421cbe38731901d050fc90f92bdffaff83e7e36
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-01-13 12:12:04 +0900

    db.serviceを作成して広告動画の情報を取得するようにした。

--- src/youtube/youtube.module.ts
+++ src/youtube/youtube.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ScrapingService } from './scraping/scraping.service';
+import { ApiService } from './api/api.service';
+import { DbService } from './db/db.service';
 
 @Module({
-  providers: [ScrapingService],
+  providers: [ScrapingService, ApiService, DbService],
 })
 export class YoutubeModule {}
